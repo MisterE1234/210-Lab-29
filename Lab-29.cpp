6fb79c5fdfe2bec0ca83ef6ff572967e37844005
MisterE1234 <MisterE1234@outlook.com>
2025-11-05 22:47:41 -0800

    5th Commit

--- Lab-29.cpp
+++ Lab-29.cpp
@@ -9,11 +9,12 @@
     //a partial constructor to have a custom party affiliation with staunch set false by default.
 
 //Define a function that would simulate what event[s] would have happend for this voting period and how they affect different voters using a probabilty matrix.
-    //Parameters: a voter, and a random int variable
-    //returns: a number
+    //Parameters: a voter, and 3 random int variables
+    //returns: an integer indicating which party they are a part of now and and if they are staunch.
 
 //Define main function:
     //Initialize a map to store party information, each associated with its different kinds of voters.
+    //declare int variables named rPop, lPop, iPop, nonPop, and rStaunch, lStaunch, iStaunch, and nonStaunch.
     
     //Open a external file to read integer to the list to become voters.
         //if it does not open, print an error message and return -1.
@@ -22,3 +23,15 @@
         // for each line extract a party affilation to make a voter.
 
     //close file
+
+    // Begin a time-based simulation for voting changes:
+        //25 time intervals
+            // run a random numbers for disaster, war, and economic condition.
+                // Iterate through each voting division
+                    // for each voter see if they are going to change party, stay, or become staunch
+                        //if they become stauch it makes it harder for them to switch parties, same if they switch to non-voter.
+                        // If they become staunch update their bool variable, if tehy were staunch and left their party set their stauch to false.
+                        //If they are changing parties, save voter data to a temporary Voter and delete the original and then place the Voter in their new party.
+                        //use the int variables to keep track of how many are in a division and how many are staunch.
+
+                    //Print the changes by displaying the current party population with staunches as well.
