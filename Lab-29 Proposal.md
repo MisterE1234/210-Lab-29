d07fdd43ac13a1548a4230defdaa7ca0f6934c33
MisterE1234 <MisterE1234@outlook.com>
2025-11-05 22:34:29 -0800

    4th Commit

--- Lab-29 Proposal.md
+++ Lab-29 Proposal.md
@@ -2,7 +2,7 @@ Project Overview:
 My project creates a voting simulation, a program designed to predict voting changes and patterns over the next 25 elections. This software will illustrate the impact of the leading party and national events have on the voting scene.
 Implementation: 
 I plan to use a std:map to store and manage data related to different voters across different voting divisions. This map will categorize voters into three std:lists representing left, moderate, and right political leanings. The map's values will be an array of these three lists.
-Two map entries would be for two main parties. another amp entry would be for a independent party. finally, a forth for non-voters.
+One map entry would be for a left party.An additional map would be for a right party. Another map entry would be for a independent party. finally, a forth for non-voters.
 Simulated events:
 The simulation will focus on four events: An economic boom, an economic downturn, a disaster (natural or otherwise), and a war.
 These events could effect voter perception and change their party affilation and voting pattern.
--- Lab-29.cpp
+++ Lab-29.cpp
@@ -4,8 +4,21 @@
 //Include necessary headers for files, data structures, processing, and ect.
 
 //Define a class named Voter:
-    //would hold a int party variable with four options (1-4).
+    //would hold a int party variable with three options (1-3). To represent left, moderate, right.
+    //an bool variable to see if the voter is staunch or not.
+    //a partial constructor to have a custom party affiliation with staunch set false by default.
 
-//Define a function that would simulate what event[s] would have happend for this voting period and how they affect different voters.
+//Define a function that would simulate what event[s] would have happend for this voting period and how they affect different voters using a probabilty matrix.
     //Parameters: a voter, and a random int variable
     //returns: a number
+
+//Define main function:
+    //Initialize a map to store party information, each associated with its different kinds of voters.
+    
+    //Open a external file to read integer to the list to become voters.
+        //if it does not open, print an error message and return -1.
+    
+    //Read data to lists: split three ways 30% R: 30% L: 40% I.
+        // for each line extract a party affilation to make a voter.
+
+    //close file
