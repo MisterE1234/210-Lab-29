9304a11e43dba60469336cc224628350a87e4fbf
MisterE1234 <MisterE1234@outlook.com>
2025-11-07 14:00:52 -0800

    5th Update

--- Lab-29.cpp
+++ Lab-29.cpp
@@ -14,6 +14,8 @@
 
 using namespace std;
 
+int VOTER_NUM = 100;  int INITIAL_R = 40; int INITIAL_L = 40; int INITIAL_I = 40;
+
 //Define a class named Voter:
     //would hold a int political leaning variable with three options (1-3). To represent left, moderate, right.
     //an bool variable to see if the voter is staunch or not.
@@ -54,22 +56,25 @@ using namespace std;
 //Define main function:
     //
     //Initialize a map to store party information, each associated with its different kinds of voters.
-    //declare int variables named rPop, lPop, iPop, nonPop, and rStaunch, lStaunch, iStaunch, and nonStaunch. Also a int variable that hold who is in charge:
+    //declare int variables to hold the population in each party and how many are staunch or non-voters. Also a int variable that hold who is in charge:
     
     int main (){
         map < string, array <list <Voter>, 3 >> polLandscape;
         int rPop = 0;
         int lPop = 0;
         int iPop = 0;
-        int nonPop = 0;
         int rStaunch = 0;
         int lStaunch = 0;
         int iStaunch = 0;
-        int nonStaunch = 0;
+        int rNon = 0;
+        int lNon = 0;
+        int iNon = 0;
         int presidentParty;
 
 
-    
+    polLandscape["Right"] = {};
+    polLandscape["Left"] = {};
+    polLandscape["Independent"] = {};
     //Open a external file to read integer to the list to become voters.
         //if it does not open, print an error message and return -1.
 
@@ -83,12 +88,17 @@ using namespace std;
         return 1;
     }
 
+    for(int i = 0; i < INITIAL_R; i++){
+    
+    }
     
     //Read data to lists: split three ways 40% R: 40% L: 20% I.
 
         // for each line extract a party affilation to make a voter.
 
+
     //close file
+    iFile.close();
 
     // Begin a time-based simulation for voting changes:
         //25 time intervals
